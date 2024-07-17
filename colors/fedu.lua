3149cfb91c5912da840aa0dfa08abebce778bd40
Dominiq <bfwnupfs@duck.com>
2024-07-17 10:43:41 +0200

    remove package.loaded hack

--- colors/fedu.lua
+++ colors/fedu.lua
@@ -14,7 +14,10 @@ vim.g.colors_name = "fedu"
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded["lush_theme.fedu"] = nil
+-- package.loaded["lush_theme.fedu"] = nil
 
 -- include our theme file and pass it to lush to apply
-require("lush")(require("lush_theme.fedu"))
+local theme = require("lush_theme.fedu")
+local lush = require("lush")
+
+lush(theme)
